6d88739c9a0c1b4628fb0251d5578d4bdf71d3cf
SnehaJarsania <sneha.jarsania@capgemini.com>
2020-07-15 18:58:30 +0530

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Sneha Jarsania on 7/15/20.
+//  Copyright © 2020 Sneha Jarsania. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
