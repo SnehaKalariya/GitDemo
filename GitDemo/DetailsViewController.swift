f80c1f48ad359203e2e0bb447eacdcc23152dca9
SnehaJarsania <sneha.jarsania@capgemini.com>
2020-07-15 19:27:18 +0530

    Initial Commit

---
+++ GitDemo/DetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsViewController.swift
+//  GitDemo
+//
+//  Created by Sneha Jarsania on 7/15/20.
+//  Copyright © 2020 Sneha Jarsania. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
