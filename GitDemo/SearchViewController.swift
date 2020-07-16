37e2a0db15ebe966cd463d6fb03cc30cc8c83849
SnehaJarsania <sneha.jarsania@capgemini.com>
2020-07-16 12:29:32 +0530

    Add Search functionality

---
+++ GitDemo/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  GitDemo
+//
+//  Created by Sneha Jarsania on 7/16/20.
+//  Copyright © 2020 Sneha Jarsania. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func searchResult()  {
+        print("result is here")
+    }
+}
